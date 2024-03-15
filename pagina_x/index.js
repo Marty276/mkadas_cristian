17cd80847fc0af52bc73c9efb93e6f63c9d8d8a8
Marty_276 <martybedoya10@gmail.com>
2024-03-15 12:25:54 -0400

    data added to index.js

---
+++ pagina_x/index.js
@@ -0,0 +1,61 @@
+/*
+TIPOS DE DATOS:
+
+int - números enteros
+float - número decimales
+str - texto
+bool - verdadero - falso | true - false | 1 - 0
+
+
+DATOS COMPUESTOS:
+
+array - lista = ["hola", 123, false, 654.512, [1, 2, 3, 4, 5]]
+
+
+key : value
+objeto - datos_cristian = {
+    nombre : "Cristian",
+    apellido : "Trujillo",
+    edad : 18,
+    altura : "165cm"
+}
+
+----------------------------------------------------
+
+
+OPERADORES:
+
+Operadores aritméticos:
+
++ - suma
+- - resta
+* - multiplicación
+/ - división
+% - modulo
+** - exponenciación
+
+Operadores de comparación:
+
+== - igual
+=== - extrictamente igual
+!= - diferente
+!== - extrictamente diferente
+< - menor
+<= - menor o igual
+> - mayor
+>= - mayor o igual
+
+
+Operadores lógicos:
+
+&& - se cumple a y b
+|| - se cumple a o b
+! - no se cumple
+
+*/
+
+/*
+
+
+
+*/
